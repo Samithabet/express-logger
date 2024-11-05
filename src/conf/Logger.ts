1ea065372e78c8cda8e25f4a177bc779121f9744
Samithabet <sami.th71516@gmail.com>
2024-11-05 15:28:41 +0300

    refactoring:complete

--- src/conf/Logger.ts
+++ src/conf/Logger.ts
@@ -1,7 +1,31 @@
-// config/logger.ts
+// // config/logger.ts
+// import { createLogger, format, transports } from 'winston';
+
+// const { combine, timestamp, json, colorize } = format;
+
+// const logger = createLogger({
+//   level: 'info',
+//   format: combine(timestamp(), json()),
+//   transports: [
+//     new transports.Console({
+//       format: combine(colorize(), format.simple()),
+//     }),
+//     new transports.File({ filename: 'logs/app.log' }),
+//   ],
+// });
+
+// export default logger;
 import { createLogger, format, transports } from 'winston';
+import AWS from 'aws-sdk';
 
 const { combine, timestamp, json, colorize } = format;
+AWS.config.update({
+  region: process.env.REGION,
+  accessKeyId: process.env.ACCESSKEYID,
+  secretAccessKey: process.env.SECRETACCESSKEY,
+});
+const s3 = new AWS.S3();
+
 
 const logger = createLogger({
   level: 'info',
@@ -10,8 +34,34 @@ const logger = createLogger({
     new transports.Console({
       format: combine(colorize(), format.simple()),
     }),
-    new transports.File({ filename: 'logs/app.log' }),
   ],
 });
+
+// Send logs to S3 in production environment
+if (process.env.NODE_ENV === 'production') {
+  logger.on('data', (log) => {
+    const bucketName = 'aman-platform-production';
+    if (!bucketName) {
+      console.error("Error: S3_BUCKET_NAME environment variable is not set.");
+      return;
+    }
+
+    const params = {
+      Bucket: bucketName,
+      Key: `logs/app-${new Date().toISOString()}.log`,
+      Body: JSON.stringify(log),
+      ContentType: 'application/json',
+    };
+
+    s3.upload(params, (err:any, data: any) => {
+      if (err) {
+        console.error('Error uploading log to S3:', err);
+      } else {
+        console.log('Log uploaded to S3:', data.Location);
+      }
+    });
+  });
+}
+
 
 export default logger;
