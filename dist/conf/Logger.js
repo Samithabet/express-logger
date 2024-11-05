37267cfd3963edcf069c69335eccebb4a9846599
Samithabet <sami.th71516@gmail.com>
2024-11-05 15:30:59 +0300

    refactoring:comp

--- dist/conf/Logger.js
+++ dist/conf/Logger.js
@@ -1,8 +1,31 @@
 "use strict";
+// // config/logger.ts
+// import { createLogger, format, transports } from 'winston';
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-// config/logger.ts
+// const { combine, timestamp, json, colorize } = format;
+// const logger = createLogger({
+//   level: 'info',
+//   format: combine(timestamp(), json()),
+//   transports: [
+//     new transports.Console({
+//       format: combine(colorize(), format.simple()),
+//     }),
+//     new transports.File({ filename: 'logs/app.log' }),
+//   ],
+// });
+// export default logger;
 const winston_1 = require("winston");
+const aws_sdk_1 = __importDefault(require("aws-sdk"));
 const { combine, timestamp, json, colorize } = winston_1.format;
+aws_sdk_1.default.config.update({
+    region: process.env.REGION,
+    accessKeyId: process.env.ACCESSKEYID,
+    secretAccessKey: process.env.SECRETACCESSKEY,
+});
+const s3 = new aws_sdk_1.default.S3();
 const logger = (0, winston_1.createLogger)({
     level: 'info',
     format: combine(timestamp(), json()),
@@ -10,7 +33,30 @@ const logger = (0, winston_1.createLogger)({
         new winston_1.transports.Console({
             format: combine(colorize(), winston_1.format.simple()),
         }),
-        new winston_1.transports.File({ filename: 'logs/app.log' }),
     ],
 });
+// Send logs to S3 in production environment
+if (process.env.NODE_ENV === 'production') {
+    logger.on('data', (log) => {
+        const bucketName = 'aman-platform-production';
+        if (!bucketName) {
+            console.error("Error: S3_BUCKET_NAME environment variable is not set.");
+            return;
+        }
+        const params = {
+            Bucket: bucketName,
+            Key: `logs/app-${new Date().toISOString()}.log`,
+            Body: JSON.stringify(log),
+            ContentType: 'application/json',
+        };
+        s3.upload(params, (err, data) => {
+            if (err) {
+                console.error('Error uploading log to S3:', err);
+            }
+            else {
+                console.log('Log uploaded to S3:', data.Location);
+            }
+        });
+    });
+}
 exports.default = logger;
--- dist/controllers/ExampleController.js
+++ dist/controllers/ExampleController.js
@@ -11,56 +11,6 @@ class ExampleController {
         LogService_1.default.logMessage('Route /example was accessed.');
         res.send('Hello, World!');
     }
-    // static getLogs(req: Request, res: Response): void {
-    //     const fs = require('fs');
-    //     const path = require('path');
-    //     const logFilePath = path.join(__dirname, '../../logs/app.json');
-    //     fs.readFile(logFilePath, 'utf8', (err: NodeJS.ErrnoException | null, data: string) => {
-    //         if (err) {
-    //             return res.status(500).json({ error: 'Could not retrieve logs' });
-    //         }
-    //         try {
-    //             const logs = data
-    //                 .split('\n')
-    //                 .filter((line: string) => line)
-    //                 .map((line: string) => JSON.parse(line));
-    //             res.json(logs);
-    //         } catch (parseError) {
-    //             res.status(500).json({ error: 'Failed to parse logs' });
-    //         }
-    //     });
-    // }
-    // static getLogs(req: Request, res: Response): void {
-    //     const logFilePath: string = path.join(__dirname, '../../logs/app.log');
-    //     // Parse page and pageSize from the query parameters, set default values
-    //     const page: number = parseInt(req.query.page as string, 10) || 1;
-    //     const pageSize: number = parseInt(req.query.pageSize as string, 10) || 30;
-    //     fs.readFile(logFilePath, 'utf8', (err: NodeJS.ErrnoException | null, data: string) => {
-    //         if (err) {
-    //             return res.status(500).json({ error: 'Could not retrieve logs' });
-    //         }
-    //         try {
-    //             const logs: Record<string, unknown>[] = data
-    //                 .split('\n')
-    //                 .filter((line: string) => line)
-    //                 .map((line: string) => JSON.parse(line));
-    //             // Calculate pagination
-    //             const startIndex: number = (page - 1) * pageSize;
-    //             const endIndex: number = startIndex + pageSize;
-    //             const paginatedLogs: Record<string, unknown>[] = logs.slice(startIndex, endIndex);
-    //             // Send paginated results along with metadata
-    //             res.json({
-    //                 logs: paginatedLogs,
-    //                 currentPage: page,
-    //                 pageSize: pageSize,
-    //                 totalLogs: logs.length,
-    //                 totalPages: Math.ceil(logs.length / pageSize),
-    //             });
-    //         } catch (parseError) {
-    //             res.status(500).json({ error: 'Failed to parse logs' });
-    //         }
-    //     });
-    // }
     static getLogs(req, res) {
         const logFilePath = path_1.default.join(__dirname, '../../logs/app.log');
         // Parse page and pageSize from the query parameters, set default values
